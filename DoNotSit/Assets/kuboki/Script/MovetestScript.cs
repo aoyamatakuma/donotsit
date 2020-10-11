8692a66ec77183b7a1eecc2b5f8622593c9a554e
taisei10520 <taisei_10520@yahoo.co.jp>
2020-10-12 07:30:54 +0900

    ステージの移動処理、ステージの枠を生成、ステージの生成

---
+++ DoNotSit/Assets/kuboki/Script/MovetestScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovetestScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.position += Vector3.up*10;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.position += Vector3.down*10;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position += Vector3.left*10;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += Vector3.right*10;
+        }
+    }
+}
