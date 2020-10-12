e64528a5197a5d3de0f1de89c75540a7f95ea79d
KPC-017A1393\K017A1393 <kabaya.k017a1393@gmail.com>
2020-10-12 14:24:29 +0900

    旧型プレイヤー

--- DoNotSit/Assets/Script/kabaya/PlayerControl.cs
+++ DoNotSit/Assets/Script/kabaya/PlayerControl.cs
@@ -6,7 +6,8 @@ public enum PlayerState
 {
     Normal,
     Charge,
-    Attack
+    Attack,
+    PowerAttack
 }
 public class PlayerControl : MonoBehaviour
 {
@@ -19,11 +20,12 @@ public class PlayerControl : MonoBehaviour
     public bool attackFlag;//アタックフラグ
     public int hp;//Hp
     public int attackCount;
+    public int chargeCount;
     public PlayerState currentPlayerState; //現在の状態
     // Start is called before the first frame update
     void Start()
     {
-        rb =  GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         jumpFlag = false;
         attackFlag = false;
         hp = 3;
@@ -57,10 +59,11 @@ public class PlayerControl : MonoBehaviour
     }
     void Jump()//ジャンプ系
     {
-        //Aボタン
-        if (Input.GetButtonDown("Jump")&& jumpFlag == false)
+        //Aボタン Input.GetButtonDown("Jump")&&
+        if (Input.GetButtonDown("Jump") && jumpFlag == false)
         {
-            rb.AddForce(Vector2.up * jumpForce);
+            // rb.AddForce(Vector2.up * jumpForce);
+            rb.velocity = Vector2.up * jumpForce;
             jumpFlag = true;
         }
     }
@@ -69,21 +72,14 @@ public class PlayerControl : MonoBehaviour
         var h1 = Input.GetAxis("Horizontal");
         var v1 = Input.GetAxis("Vertical");
         //Bボタン
-        if (Input.GetButtonDown("Attack"))
+        if (Input.GetButtonUp("Attack"))
         {
             if (h1 != 0 || v1 != 0)
             {
-                transform.position +=  new Vector3((h1*attackForce) * Time.deltaTime, (v1*attackForce) * Time.deltaTime, 0);
-                attackTime = 0;
+                transform.position += new Vector3(h1 * Time.deltaTime, v1 * Time.deltaTime, 0) * attackForce;
                 attackFlag = true;
+                currentPlayerState = PlayerState.Attack;
             }
-            //else
-            //{
-            //    transform.position += new Vector3(attackForce * Time.deltaTime, 0, 0);
-            //    attackTime = 0;
-            //    attackFlag = true;
-            //}
-            currentPlayerState = PlayerState.Attack;
         }
         else
         {
@@ -96,30 +92,29 @@ public class PlayerControl : MonoBehaviour
         var h1 = Input.GetAxis("Horizontal");
         var v1 = Input.GetAxis("Vertical");
         //長押しBボタン
-        if (Input.GetButton("Attack"))
+        if (Input.GetButton("Attack") && attackFlag == false)
         {
-            attackTime += Time.deltaTime;
+            chargeCount++;
             currentPlayerState = PlayerState.Charge;
         }
-        else if (Input.GetButtonUp("Attack")&& attackTime >=3.0f)
+        else if (Input.GetButtonUp("Attack") && attackCount <= chargeCount && attackFlag == false)
         {
-               if (h1 != 0 || v1 != 0)
+            if (h1 != 0 || v1 != 0)
             {
-                transform.position +=  new Vector3((h1 * attackForce) * Time.deltaTime, (v1 * attackForce) * Time.deltaTime, 0) * attackTime;
-                attackTime = 0;
+                transform.position += new Vector3(h1 * Time.deltaTime, v1 * Time.deltaTime, 0) * (attackForce * 2);
                 attackFlag = true;
             }
-               else
-            {
-                transform.position += new Vector3(attackForce * Time.deltaTime, 0, 0) * attackTime;
-                attackTime = 0;
-                attackFlag = true;
-            }
-            currentPlayerState = PlayerState.Attack;
+            //else
+            //{
+            //    transform.position += new Vector3(attackForce * Time.deltaTime, 0, 0) * 2 ;
+            //    attackFlag = true;
+            //}
+            currentPlayerState = PlayerState.PowerAttack;
         }
         else
         {
             attackFlag = false;
+            chargeCount = 0;
             currentPlayerState = PlayerState.Normal;
         }
     }
