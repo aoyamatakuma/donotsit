e322dd6a5e2cf301f7256eb6249eeff703aad2b5
VANO0125 <43366986+VANO0125@users.noreply.github.com>
2020-10-23 15:40:43 +0900

    .add タイトルシーンとステージセレクト変更＆エネミーの当たり判定追加

--- DoNotSit/Assets/Script/SceneScript/GameOverScene.cs
+++ DoNotSit/Assets/Script/SceneScript/GameOverScene.cs
@@ -2,21 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverScene : MonoBehaviour
 {
+    public GameObject stageSelect;
+    public GameObject title;
+    private bool select;
     // Start is called before the first frame update
     void Start()
     {
-
+        select = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        Select();
+        if (Input.GetKey(KeyCode.Space) || Input.GetButton("Jump"))
         {
-            SceneManager.LoadScene("Title");
+            if (select)
+            {
+                SceneManager.LoadScene("StageSelect");
+            }
+            else
+            {
+                SceneManager.LoadScene("Title");
+            }
         }
     }
+
+    void Select()
+    {
+        float ver = Input.GetAxis("Vertical");
+
+        if (select)
+        {
+            stageSelect.GetComponent<Outline>().enabled = true;
+            title.GetComponent<Outline>().enabled = false;
+        }
+        else
+        {
+            stageSelect.GetComponent<Outline>().enabled = false;
+            title.GetComponent<Outline>().enabled = true;
+        }
+
+        if (ver > 0.5f)
+        {
+            select = true;
+
+        }
+        else if (ver < -0.5f)
+        {
+            select = false;
+        }
+
+    }
+
 }
