3214b3816c9a11083f4c3cc82c2a7872435762f0
taisei10520 <taisei_10520@yahoo.co.jp>
2020-10-28 00:50:39 +0900

    ポーズ画面作成、inputmanager追加（Select）

--- DoNotSit/Assets/kuboki/Script/Pouse/PouseButtonScript.cs
+++ DoNotSit/Assets/kuboki/Script/Pouse/PouseButtonScript.cs
@@ -2,19 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PouseButtonScript : MonoBehaviour
 {
+    Fade fade;
+    public GameObject end;
+    public GameObject title;
+    private AudioSource audio;
+    private bool select;
+    public AudioClip selectSE;
+    public AudioClip moveSE;
     // Start is called before the first frame update
     void Start()
     {
-        
+        fade = GetComponent<Fade>();
+        audio = GetComponent<AudioSource>();
+        select = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Select();
+        if (Input.GetKey(KeyCode.Space) || Input.GetButtonDown("Jump"))
+        {
+            audio.PlayOneShot(selectSE);
+            if (select)
+            {
+
+                endClick();
+            }
+            else
+            {
+                fade.StartFadeIn("Title", true);
+            }
+        }
+    }
+    void Select()
+    {
+        float ver = Input.GetAxis("Vertical");
+
+        if (!select)
+        {
+            end.GetComponent<Outline>().enabled = false;
+            title.GetComponent<Outline>().enabled = true;
+
+        }
+        else
+        {
+            end.GetComponent<Outline>().enabled = true;
+            title.GetComponent<Outline>().enabled = false;
+        }
+
+        if (ver > 0.5f)
+        {
+            if (!select)
+            {
+                audio.PlayOneShot(moveSE);
+            }
+            select = false;
+
+        }
+        else if (ver < -0.5f)
+        {
+            if (select)
+            {
+                audio.PlayOneShot(moveSE);
+            }
+            select = true;
+        }
+
     }
 
     public void endClick()
@@ -24,9 +82,5 @@ public class PouseButtonScript : MonoBehaviour
 #elif UNITY_STANDALONE
         UnityEngine.Application.Quit();
 #endif
-    }
-    public void titleClick()
-    {
-        SceneManager.LoadScene("Title");
     }
 }
