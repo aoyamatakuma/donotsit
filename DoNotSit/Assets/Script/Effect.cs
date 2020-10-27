548e92c66ee00d52e8c9756037f4c28e53b1c1bb
VANO0125 <43366986+VANO0125@users.noreply.github.com>
2020-10-28 01:38:20 +0900

    .add エフェクトいろいろ追加

---
+++ DoNotSit/Assets/Script/Effect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect : MonoBehaviour
+{
+    public float lifeTime;
+    private float cnt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cnt = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cnt += Time.deltaTime;
+        if(cnt > lifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
