62e914acd34dfb7ee306bfadbb0e99be82b18b4f
KPC-017A1393\K017A1393 <kabaya.k017a1393@gmail.com>
2020-10-28 20:28:18 +0900

    縦移動ブロック追加

---
+++ DoNotSit/Assets/MoveHeight.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveHeight : MonoBehaviour
+{
+    public float speed;
+    public bool isReturn;
+    public float timer;//タイマー
+    public float returntimer;
+    void Start()
+    {
+        isReturn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move(speed);
+        timer += 1.0f * Time.deltaTime;
+        Timer();
+    }
+
+
+    public void Move(float speed)
+    {
+        Vector3 pos = transform.position;
+        if (isReturn == false)
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+        else if (isReturn == true)
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+        transform.position = pos;
+
+    }
+    void Timer()
+    {
+        if (timer >= returntimer && isReturn == false)
+        {
+            timer = 0;
+            isReturn = true;
+        }
+        if (timer >= returntimer && isReturn == true)
+        {
+            timer = 0;
+            isReturn = false;
+        }
+    }
+}
