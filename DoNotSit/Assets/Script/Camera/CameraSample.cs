1fcfce8e6d4f2302d533b438e1f67dbc2c0c9754
VANO0125 <43366986+VANO0125@users.noreply.github.com>
2020-11-02 11:42:31 +0900

    .add 強制スクロール仮追加

---
+++ DoNotSit/Assets/Script/Camera/CameraSample.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSample : MonoBehaviour
+{
+    public float speed;
+    public GameObject player;
+    public float delay;
+    float cnt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(player.transform.position.x,
+            transform.position.y,
+            transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cnt += Time.deltaTime;
+        if(cnt > delay)
+        {
+            Vector3 move = transform.position;
+            move.x += speed * Time.deltaTime;
+            transform.position = move;
+        }
+      
+    }
+}
