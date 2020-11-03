1c050dfab42c32ac8627750e540e43dcf6dabf6d
VANO0125 <43366986+VANO0125@users.noreply.github.com>
2020-11-03 14:35:14 +0900

    .add ブロック消えたり出たりするやつ

---
+++ DoNotSit/Assets/Script/BlockElement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockElement : MonoBehaviour
+{
+    public float elementNum;
+
+    public void Draw()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Delete()
+    {
+        gameObject.SetActive(false);
+    }
+
+
+}
