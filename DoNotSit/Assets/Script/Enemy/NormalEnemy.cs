73bb2d3bd0a7856dbcbb5f3c8c3c9b89e49e8c26
VANO0125 <43366986+VANO0125@users.noreply.github.com>
2020-11-13 10:54:57 +0900

    .update script修正

--- DoNotSit/Assets/Script/Enemy/NormalEnemy.cs
+++ DoNotSit/Assets/Script/Enemy/NormalEnemy.cs
@@ -20,7 +20,6 @@ public class NormalEnemy : NormalEnemyAI
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("触っているか" + isTouch);
         if (!isDead)
         {
             base.Move(speed);
