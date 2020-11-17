dac863870e9eccf851d19a87d056ffe6f1265ce8
KPC-017A1393\K017A1393 <kabaya.k017a1393@gmail.com>
2020-11-17 18:15:25 +0900

    ステージごとのランク付け

---
+++ DoNotSit/Assets/Script/RankManger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RankManger : MonoBehaviour
+{
+    public float S;
+    public float A;
+    public float B;
+    public static string rank;
+    private float playerScore;
+    // Update is called once per frame
+    void Update()
+    {
+        playerScore = PlayerControl.scoreNumber;
+        if (playerScore >= S)//Sランク
+        {
+            rank = "S";
+        }
+        if (playerScore < S)//Aランク
+        {
+            rank = "A";
+        }
+        if (playerScore <= A)//Bランク
+        {
+            rank = "B";
+        }
+        if (playerScore <= B)//Cランク
+        {
+            rank = "C";
+        }
+       // Debug.Log(rank);
+    }
+    public static string ClearRank()
+    {
+        return rank;
+    }
+}
