16420be88709ef7ab1f22fe062bf90c1e1da3a3f
KPC-017A1393\K017A1393 <kabaya.k017a1393@gmail.com>
2020-11-19 13:44:43 +0900

    オプション土台

---
+++ DoNotSit/Assets/Script/OptionManger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OptionManger : MonoBehaviour
+{
+    Fade fade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fade = GetComponent<Fade>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKey(KeyCode.Escape) || Input.GetButtonDown("Attack")))
+        {
+            fade.StartFadeIn("Title", true);
+        }
+    }
+}
