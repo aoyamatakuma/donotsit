afa9ef3c20cd7c479633c46bec8e088275805d45
VANO0125 <43366986+VANO0125@users.noreply.github.com>
2020-12-04 13:11:00 +0900

    .add ロック解除SE追加

---
+++ DoNotSit/Assets/Script/Manager/AnimationSE.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSE : MonoBehaviour
+{
+    AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Play()
+    {
+        audio.Play();
+    }
+}
