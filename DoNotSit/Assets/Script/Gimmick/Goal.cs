02833758a28b0c8cbe16acc5b2478bb20b15ee8d
VANO0125 <43366986+VANO0125@users.noreply.github.com>
2020-12-11 10:26:25 +0900

    .add ゴール演出追加

--- DoNotSit/Assets/Script/Gimmick/Goal.cs
+++ DoNotSit/Assets/Script/Gimmick/Goal.cs
@@ -2,22 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
 
 public class Goal : MonoBehaviour
 {
     Fade fade;
+    public PlayableDirector playableDirector;
+    private GameObject player;
+    private GameObject chaseEnemy;
+    private bool isEvent;
+    private GameObject camera;
 
     void Start()
     {
         fade = GetComponent<Fade>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        chaseEnemy = GameObject.FindGameObjectWithTag("ChaseEnemy");
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
     }
+
+    void Update()
+    {
+        if (isEvent)
+        {
+            ExitMovie();
+        }
+    }
+
 
     void OnTriggerEnter(Collider col)
     {
         if(col.gameObject.tag == "Player")
-        {
+        {    
             StageDate.Instance.SetData(SceneManager.GetActiveScene().name);
-            fade.StartFadeIn("GameClear",false);
+            StartMovie();
+        }
+    }
+
+    void StartMovie()
+    {
+        player.SetActive(false);
+        chaseEnemy.SetActive(false);
+        camera.SetActive(false);
+        playableDirector.Play();
+        isEvent = true;
+    }
+
+    void ExitMovie()
+    {
+        if (playableDirector.state != PlayState.Playing)
+        {
+            playableDirector.Stop();
+            fade.StartFadeIn("GameClear", false);
         }
     }
 }
