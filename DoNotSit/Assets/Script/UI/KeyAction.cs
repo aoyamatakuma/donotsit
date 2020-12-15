b938c7fb1e94940f32acfe164b0e3a639c67e56d
VANO0125 <43366986+VANO0125@users.noreply.github.com>
2020-12-15 15:00:51 +0900

    .add キーボード変更

---
+++ DoNotSit/Assets/Script/UI/KeyAction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyAction : MonoBehaviour
+{
+    public Color pushColor;
+    private Image image;
+    private Color baseColor;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        baseColor = image.color;
+    }
+
+    public void Push()
+    {
+        StartCoroutine(PushCoroutine());
+    }
+   
+    IEnumerator PushCoroutine()
+    {
+        image.color = pushColor;
+        yield return new WaitForSeconds(0.5f);
+        image.color = baseColor;
+    }
+
+    public void Reset()
+    {
+        image.color = baseColor;
+    }
+}
